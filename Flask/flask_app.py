1907d777c16f7b436e8e9d70b81960499516f9b2
felipexpinho <felipe_pinho8@hotmail.com>
2024-12-24 17:33:10 -0300

    refactor(Flask): refactor app.py for better structure

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -1,31 +1,11 @@
-from flask import Flask, g
-from routes.store import store_blueprint
-from database.session import Base, engine, SessionLocal
-
 # Import all models to register them with Base.metadata
 from models.store import Store
 from models.product import Product
 from models.stock import Stock
 
-app = Flask(__name__)
+from utils.create_app import create_app
 
-# Register blueprints
-app.register_blueprint(store_blueprint, url_prefix="/store")
-
-@app.before_request
-def create_db_session():
-    """Create a new database session before each request."""
-    g.db = SessionLocal()
-
-@app.teardown_request
-def close_db_session(exception=None):
-    """Close the database session after each request."""
-    db = g.pop("db", None)
-    if db:
-        db.close()
+app = create_app()
 
 if __name__ == "__main__":
-    # Create all tables
-    Base.metadata.create_all(bind=engine)
-
     app.run(debug=True, port=8000)
